749299f08d7be55ceeb3380905833548c258d673
sameerapr <sameerapraditha.ati@gmail.com>
2023-06-10 13:48:31 +0530

    add data into model class and show model class data

--- bin/learn_model_class.dart
+++ bin/learn_model_class.dart
@@ -1,41 +1,50 @@
-
-import 'dart:convert';
-
-import 'package:learn_model_class/learn_model_class.dart' as learn_model_class;
-
 void main() {
-   var mapObj = {
-  "name":"sameera",
-  "age":34,
-  "country":"Sri Lanka",
+  
+  //create instance to User model class set values for class varialbles
+  User user = User(name: 'Senitha', age:2, country: 'Kurunegala');
+  print(user.name);
+  print(user.toJson());
+
+  //create map object
+  var mapObj = {
+    "name": "sameera",
+    "age": 34,
+    "BornLocation": "Mathara",
   };
 
   print(mapObj);
 
-
-  User user = User();
-
+  //pass map object values into User model class
   user = User.fromJson(mapObj);
+  //print value from User model class
   print(user.name);
 
 }
 
 class User {
-   String? name ;
-   int? age ;
-   String? country;
+  String name;
+  int? age;
+  String? country;
 
   User({
-     this.name,
-     this.age,
-     this.country,
+    required this.name,
+    this.age,
+    this.country,
   });
 
-    factory User.fromJson(Map<String,dynamic>json){
+  factory User.fromJson(Map<String, dynamic> json) {
     return User(
-      name:json['name'],
-      age:json['age'],
-      country: json['country'],
+      name: json['name'],
+      age: json['age'],
+      country: json['BornLocation'],
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['name'] = name;
+    data['age'] = age;
+    data['BornLocation'] = country;
+    return data;
   }
 }
